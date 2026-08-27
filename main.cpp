3f2b1ed8334b8e3a72aa3d1652906e41aa953eda
dahfluff <dahfluffco@gmail.com>
2026-08-27 16:30:44 +0000

    added w

--- main.cpp
+++ main.cpp
@@ -5,7 +5,20 @@ using namespace std;
 
 int main()
 {
+	double val1;
+	double val2;
+
+	cout << "Enter your first value" << endl;
+	cin >> val1;
+
+	cout << "Enter your second value" << endl;
+	cin >> val2;
+
+	cout << "Equation: " << val1 << " * " << val2 << "\n" << "Your product of this equation is " << val1*val2 << "." << endl;
+
+	return 0;
 	//create a few starter objects
 	//add some values to test out your values methods
 	//print out all the values for each object to validate your code
+	
 }
